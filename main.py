449c66af0bd4b025f966ca61b1114ec57038ed27
ridoineel <ridoineelofficiel@gmail.com>
2022-08-29 00:15:05 +0000

    update main.py

--- main.py
+++ main.py
@@ -3,47 +3,46 @@
 import cv2
 import numpy as np
 from scipy import signal as sg
+from PIL import Image, ImageFilter
 import math
 from os.path import join, dirname
 from utils.Class import Color, Style
+import matplotlib.pyplot as plt
 
-# difference between 
-# two images in 
-# blur mode
-IMG_DIFFERENCE = 800_000
-IMG_DIFFERENCE_AVG = 2.60416667
+MAX_IMGS_MATCH_DIST = 1850
 
 class Kernel:
 	blur = np.ones((3, 3))/9
-	egdes1 = np.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]])
+	edges1 = np.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]])
 
 def saveCurFrame(frame, filename="capture.jpg"):
 	_dir = dirname(__file__)
 
-	cv2.imwrite(join(_dir, "dist/images", filename), frame)	
+	cv2.imwrite(join(_dir, "dist/images", filename), frame)
+
+def prepareImgs(img):
+	if len(img.shape) == 3: #rgb
+		img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+	img = sg.convolve2d(img, Kernel.blur, mode="same", boundary="fill")
+
+	return img
 
 def motion(img1, img2) -> bool:
 	if len(img1.shape) not in [1, 3] or len(img2.shape) not in [1, 3]:
 		return
 
-	if len(img1.shape) == 3: #rgb
-		img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-
-	if len(img2.shape) == 3: #rgb
-		img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
-
 	if img1.shape != img2.shape:
 		return
 
-	img1 = sg.convolve2d(img1, Kernel.blur, mode="same", boundary="fill") 
-	img2 = sg.convolve2d(img2, Kernel.blur, mode="same", boundary="fill")
+	img1 = prepareImgs(img1)
+	img2 = prepareImgs(img2)
 
-	diff = np.sum(abs(img1 - img2))
-	diff_avg = diff/(img1.shape[0]*img1.shape[1])
+	# dist between img1 and img2
+	dist = np.linalg.norm(img1 - img2)
+	print(dist)
 
-	# return diff >= IMG_DIFFERENCE:
-
-	return diff_avg >= IMG_DIFFERENCE_AVG
+	return dist >= MAX_IMGS_MATCH_DIST
 
 def main():
 	# define a video capture object
@@ -58,6 +57,7 @@ def main():
 
 		if prev_frame is not None and motion(cur_frame, prev_frame):
 				print(Color.primary(f"[MOTION_DETECTOR]: motion {i + 1} detected"))
+				
 				saveCurFrame(cur_frame, f'capture{i}.jpg')
 				i += 1
 
